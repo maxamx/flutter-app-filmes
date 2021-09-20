4d3aebb23adab4053f679c03794033a4a92ff83d
maxamx <maxamx@gmail.com>
2021-09-20 15:34:00 -0300

    Stack

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-  const LoginPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Login Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            width: 200,
+            height: 200,
+          ),
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+          )
+        ],
+      ),
+    );
   }
 }
