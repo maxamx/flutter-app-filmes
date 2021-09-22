2c4682a8d2aeaa1cd027200765ea4b7fb4e6fd14
maxamx <maxamx@gmail.com>
2021-09-21 22:26:23 -0300

    filmes

--- lib/modules/filmes/filme_page.dart
+++ lib/modules/filmes/filme_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_filmes/modules/filmes/widgets/filter_tag.dart';
 import 'package:flutter/material.dart';
 
 class FilmePage extends StatelessWidget {
@@ -5,8 +6,15 @@ class FilmePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Filmes'),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              FilterTag(),
+            ],
+          )),
     );
   }
 }
---
+++ lib/modules/filmes/widgets/filmes_header.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FilmesHeader extends StatelessWidget {
+  const FilmesHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: Get.width,
+        height: 196,
+        child: Stack(children: [
+          SizedBox(
+            width: Get.width,
+            child: Image.asset(
+              'assets/images/header.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: Get.width * .9,
+            padding: EdgeInsets.only(bottom: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: 'Procurar filmes',
+                  labelStyle: TextStyle(fontSize: 15, color: Colors.grey),
+                  prefixIcon: Icon(Icons.search),
+                  contentPadding: EdgeInsets.zero,
+                  floatingLabelBehavior: FloatingLabelBehavior.never),
+            ),
+          )
+        ]));
+  }
+}
